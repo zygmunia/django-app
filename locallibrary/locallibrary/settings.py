28a80fcaff535f462ae14a14c4faa2c3b73306d2
Ian Zygmunt <zygmunia@dukes.jmu.edu>
2025-04-01 23:00:02 -0400

    commit

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+
+from django.core.management.utils import get_random_secret_key
+from pathlib import Path
 import os # needed by code below
 import sys
 import dj_database_url
-from django.core.management.utils import get_random_secret_key
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
